d691e1657031756215ee220e86653018dfc0c493
Kartik4878 <66543940+Kartik4878@users.noreply.github.com>
2024-09-06 20:47:47 +0530

    Update scriptLoader.js

--- Script/scriptLoader.js
+++ Script/scriptLoader.js
@@ -1,1 +1,17 @@
+function loadJSFiles(){
+    loadFile("./configs.js");
+    loadFile("https://unpkg.com/aos@2.3.1/dist/aos.js");
+    loadFile("./Script/Components/components.js");
+    loadFile("./Script/Components/componentHandler.js")
+    loadFile("./Script/utils.js");
+    setTimeout(()=>loadFile("./Script/script.js"),100);
+}
+function loadFile(url){
+    const script = document.createElement("script");
+    script.setAttribute("src",url);
+    script.setAttribute("defer","defer");
+    document.head.appendChild(script);
+}
+
+loadJSFiles();
 
