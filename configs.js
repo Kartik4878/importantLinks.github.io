a48fdbf65b28aee42d89517fcddf43876b393624
Kartik4878 <66543940+Kartik4878@users.noreply.github.com>
2024-09-06 20:38:26 +0530

    Create configs.js

---
+++ configs.js
@@ -0,0 +1,181 @@
+const configData = {
+  "Name": "Kartik",
+  "ImportantItems": [
+    {
+      "heading": "Pega v7",
+      "content": "KartikP - Rules@321",
+      "id": "I1"
+    },
+    {
+      "heading": "Traces",
+      "content": "KartikPatel - Rules@112233",
+      "id": "I41"
+    },
+    {
+      "heading": "Commsec Dev",
+      "content": "patelk18 - rules",
+      "id": "I24"
+    },
+    {
+      "heading": "Commsec Test",
+      "content": "Kartik - Rules",
+      "id": "I63"
+    },
+    {
+      "heading": "Digidocs ",
+      "content": "Kartikdd - Rules@1122",
+      "id": "I60"
+    },
+    {
+      "heading": "PDM",
+      "content": "kartik.patel@cba.com.au  - Rules@4878",
+      "id": "I94"
+    },
+    {
+      "heading": "Commc account",
+      "content": "20005464564",
+      "id": "I53"
+    },
+    {
+      "heading": "Pega Academy",
+      "content": "ShivRatan@13",
+      "id": "I0"
+    },
+    {
+      "heading": "Digidocs 2",
+      "content": "ModelDeveloper - rules@123",
+      "id": "I12"
+    },
+    {
+      "heading": "My Know Academy",
+      "content": "JaiSoni02 - MyKnowAcedemy@123",
+      "id": "I70"
+    },
+    {
+      "heading": "Commsec Stage",
+      "content": "00516129 - Pega@4321",
+      "id": "I82"
+    }
+  ],
+  "Containers": [
+    {
+      "id": "C0",
+      "links": [
+        {
+          "id": "C0L0",
+          "address": "https://cbaess-bimowf-dt1.privatelink.pegaservice.net/prweb",
+          "label": "Commsec Dev"
+        },
+        {
+          "id": "C0L1",
+          "address": "https://cbaess-bimowf-dt2.privatelink.pegaservice.net/prweb/app/default/beEBp4uRVTogorRwSwWqbOtn9IL2fwdI*/!STANDARD",
+          "label": "Commsec Test"
+        },
+        {
+          "id": "C0L2",
+          "address": "https://cbaess-bimowf-stg1.privatelink.pegaservice.net/prweb/app/bimo_/AT7XuWLpYcMnonxn8jPIFQ*/!STANDARD",
+          "label": "Commsec Stage"
+        },
+        {
+          "id": "C0L3",
+          "address": "https://cbaess-devops.pega.net/prweb/app/default/beEBp4uRVTogorRwSwWqbOtn9IL2fwdI*/!STANDARD",
+          "label": "PDM"
+        }
+      ],
+      "label": "Pega"
+    },
+    {
+      "id": "C1",
+      "links": [
+        {
+          "id": "C1L0",
+          "address": "https://customer-relay-dev.cmp.dev.cba/prweb/app/CreditApplication_/I0GB8xujDLiisYGz1YmDUdtcc7w-2LS2GM1bfJki7hpBwws98xcVmg*/!STANDARD?pzPostData=-1367177241",
+          "label": "Relay App"
+        },
+        {
+          "id": "C1L1",
+          "address": "https://wm-eform-dev.cmp.dev.cba/prweb/a3XSU8qPU0mWjc2dGZkY8nUmfaBp4qxN*/!STANDARD?pzPostData=-751554235",
+          "label": "DEM v7"
+        },
+        {
+          "id": "C1L2",
+          "address": "https://cbaess-dem-dt1.privatelink.pegaservice.net/prweb/app/DEM/a3XSU8qPU0mWjc2dGZkY8nUmfaBp4qxN*/!STANDARD?pzPostData=-1275062921",
+          "label": "DEM v8"
+        },
+        {
+          "id": "C1L3",
+          "address": "https://wm-psutraces-dev.cmp.dev.cba/prweb/app/PaymentOperations-New/Mx0JabaftoeshhT-0bYDmsaVDK5zKTgi*/!STANDARD?pzPostData=41232424",
+          "label": "Traces"
+        },
+        {
+          "id": "C1L4",
+          "address": "https://cbaess-bimowf-dt1.privatelink.pegaservice.net/prweb/app/BIMODigiDocs_/wWIjRkxZUZo5FIXUIGtQlc318F9rVbJk*/!STANDARD?pzPostData=220329705",
+          "label": "Digi docs"
+        }
+      ],
+      "label": "Other Pega Apps"
+    },
+    {
+      "id": "C2",
+      "links": [
+        {
+          "id": "C2L0",
+          "address": "https://commbank.atlassian.net/wiki/spaces/FCDE/pages/382962049/Commsec",
+          "label": "Commsec"
+        },
+        {
+          "id": "C2L1",
+          "address": "https://commbank.atlassian.net/wiki/spaces/FCDE/pages/382962068/BIMO-Pega+Desgin+Council",
+          "label": "Pega council"
+        },
+        {
+          "id": "C2L2",
+          "address": "https://commbank.atlassian.net/wiki/spaces/FCDE/pages/662481978/COODE+UDT1+-+Sprint+24.3.181",
+          "label": "Code Reviews"
+        },
+        {
+          "id": "C2L3",
+          "address": "https://commbank.atlassian.net/wiki/spaces/FCDE/pages/675739193/COO+Pega+Team+Completed+Task+List",
+          "label": "Weekly updates"
+        },
+        {
+          "id": "C2L4",
+          "address": "https://commbank.atlassian.net/wiki/spaces/FCDE/pages/642025811/US+-+Part+1",
+          "label": "New 1.1 us"
+        }
+      ],
+      "label": "Confluence"
+    },
+    {
+      "id": "C3",
+      "links": [
+        {
+          "id": "C3L0",
+          "address": "https://commbank.sharepoint.com/sites/onecba-home",
+          "label": "One.CBA"
+        },
+        {
+          "id": "C3L1",
+          "address": "https://commbank.atlassian.net/jira/software/c/projects/COODE5/boards/59190/",
+          "label": "Jira"
+        },
+        {
+          "id": "C3L2",
+          "address": "https://proxy.mywebsso.cba/identitymanager/",
+          "label": "Identity Manager"
+        },
+        {
+          "id": "C3L3",
+          "address": "https://www.myknowacademy.com/members/jaisoni02/courses/",
+          "label": "My Know Academy"
+        },
+        {
+          "id": "C3L4",
+          "address": "https://commbank-my.sharepoint.com/:x:/r/personal/oliver_wu_cba_com_au/_layouts/15/Doc2.aspx?action=edit&sourcedoc=%7B9837275b-2cce-4b54-9522-9930d84228d4%7D&wdOrigin=TEAMS-MAGLEV.undefined_ns.rwc&wdExp=TEAMS-TREATMENT&wdhostclicktime=1709702099815&web=1",
+          "label": "Leave Calender"
+        }
+      ],
+      "label": "Others"
+    }
+  ]
+}
